025a3562619b2d0411cb1053676b5601ad5f0e06
Irina144 <solnechnayamichelle@gmail.com>
2025-11-08 17:56:23 +0300

    Initial commit

--- src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
@@ -16,7 +16,7 @@ public class CardDeliveryTest {
     @BeforeEach
     void setUp() {
         Configuration.holdBrowserOpen = false;
-        open("http://localhost:9999");
+        open("http://localhost:7777");
     }
 
     @Test
@@ -24,7 +24,6 @@ public class CardDeliveryTest {
         String planningDate = LocalDate.now().plusDays(3)
                 .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
-        // заполняем форму
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id=date] input").doubleClick().sendKeys(planningDate);
         $("[data-test-id=name] input").setValue("Иван Петров");
@@ -32,7 +31,6 @@ public class CardDeliveryTest {
         $("[data-test-id=agreement]").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
 
-        // проверяем уведомление
         $("[data-test-id=notification]")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
